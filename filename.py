32e53707906b426d155ba1ef19834606f76c9978
hani0714 <43195325+hani0714@users.noreply.github.com>
2021-12-19 01:17:22 +0900

    Update filename.py

--- filename.py
+++ filename.py
@@ -1,12 +1,12 @@
 import os
-file_path = 'txt'
+file_path = 'txt' #파일 생성할 경로
 file_names = os.listdir(file_path)
 file_names
 
 i = 1
 for name in file_names:
     src = os.path.join(file_path, name)
-    dst = str(format(i, '04')) + '.txt'
+    dst = str(format(i, '04')) + '.txt' #'.jpg' 등 원하는 확장자
     dst = os.path.join(file_path, dst)
     os.rename(src, dst)
     i += 1
